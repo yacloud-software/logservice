a52c4d23e4b1ce3bbe42a0caeb18a63e549d121c
Conrad Wood <cnw@conradwood.net>
2023-03-28 07:27:25 +0100

    updated to go-easyops version 16845

--- src/golang.conradwood.net/apis/logservice/create_1.go
+++ src/golang.conradwood.net/apis/logservice/create_1.go
@@ -1,8 +1,15 @@
 // client create: LogServiceClient
+/*
+  Created by /home/cnw/devel/go/go-tools/src/golang.conradwood.net/gotools/protoc-gen-cnw/protoc-gen-cnw.go
+*/
+
 /* geninfo:
    filename  : protos/golang.conradwood.net/apis/logservice/logservice.proto
    gopackage : golang.conradwood.net/apis/logservice
    importname: ai_0
+   clientfunc: GetLogService
+   serverfunc: NewLogService
+   lookupfunc: LogServiceLookupID
    varname   : client_LogServiceClient_0
    clientname: LogServiceClient
    servername: LogServiceServer
@@ -33,7 +40,7 @@ func GetLogClient() LogServiceClient {
        return client_LogServiceClient_0
     }
 
-    client_LogServiceClient_0 = NewLogServiceClient(client.Connect("logservice.LogService"))
+    client_LogServiceClient_0 = NewLogServiceClient(client.Connect(LogServiceLookupID()))
     lock_LogServiceClient_0.Unlock()
     return client_LogServiceClient_0
 }
@@ -49,8 +56,9 @@ func GetLogServiceClient() LogServiceClient {
        return client_LogServiceClient_0
     }
 
-    client_LogServiceClient_0 = NewLogServiceClient(client.Connect("logservice.LogService"))
+    client_LogServiceClient_0 = NewLogServiceClient(client.Connect(LogServiceLookupID()))
     lock_LogServiceClient_0.Unlock()
     return client_LogServiceClient_0
 }
 
+func LogServiceLookupID() string { return "logservice.LogService" } // returns the ID suitable for lookup in the registry. treat as opaque, subject to change.
